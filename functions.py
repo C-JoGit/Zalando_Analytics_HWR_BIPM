e8c50a04ff46ae67d03ac45edc3c5e16024e982f
C-JoGit <chisomuseni@gmail.com>
2021-04-26 11:34:56 +0200

    Exercise3 upload

--- functions.py
+++ functions.py
@@ -1,15 +1,9 @@
 '''
 Instructions/Notes
-
 Various functions and libraries for the project are imported or defined in this file, to avoid a lot of information on the project file.
-
 Use conda install (instead of pip install) on your command/terminal prompt to install packages such as : conda install gensim, conda install spacy, conda install python-Levenshtein, conda install etc...
-
 For older python version, import en_core_web_sm works, for newer version - import spacy instead also install this on terminal/command prompt 'python -m spacy download en_core_web_sm' to enable this code
-
-
 '''
-
 import pandas as pd
 import re
 from gensim.parsing.preprocessing import STOPWORDS, strip_tags, strip_numeric, strip_punctuation, strip_multiple_whitespaces, remove_stopwords, strip_short, stem_text
